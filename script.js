58568b365c30a67001af5f37fb10f92ebb739446
Riley Andersen <ande0594@algonquinlive.com>
2024-08-09 12:46:12 -0400

    fixed typo

--- script.js
+++ script.js
@@ -132,35 +132,35 @@ const questions = [
 ];
 
 const imageUrls = [
-            'https://i.pinimg.com/736x/18/36/5c/18365c5ebe02b5ac2fac228f60f2e864.jpg',
-            'https://i.pinimg.com/736x/18/36/5c/18365c5ebe02b5ac2fac228f60f2e864.jpg',
-            'https://i.pinimg.com/736x/33/5e/4c/335e4caed499f52d33a117c166715a1d.jpg',
-            'https://i.pinimg.com/736x/33/5e/4c/335e4caed499f52d33a117c166715a1d.jpg',
-            'https://i.pinimg.com/736x/ff/a6/e9/ffa6e9d7d296ce9ad45ff62b3a61d741.jpg',
-            'https://i.pinimg.com/736x/ff/a6/e9/ffa6e9d7d296ce9ad45ff62b3a61d741.jpg',
-            'https://i.pinimg.com/736x/ca/17/85/ca1785ef2e957e5bbbcf7c767eefc7e2.jpg',
-            'https://i.pinimg.com/736x/ec/1b/16/ec1b1600f90eddcede9f865671e31fb6.jpg',
-            'https://i.pinimg.com/736x/ee/db/22/eedb228b057279e3cabebfcef7ebbdf1.jpg',
-            'https://i.pinimg.com/736x/ee/db/22/eedb228b057279e3cabebfcef7ebbdf1.jpg',
-            'https://i.pinimg.com/736x/ee/b7/7f/eeb77fb88ceb4731b01bd394760cade1.jpg',
-            'https://i.pinimg.com/736x/ee/b7/7f/eeb77fb88ceb4731b01bd394760cade1.jpg',
-            'https://i.pinimg.com/736x/ee/b7/7f/eeb77fb88ceb4731b01bd394760cade1.jpg',
-            'https://i.pinimg.com/736x/29/2a/87/292a877c6aad678dabac032fd80bdb72.jpg',
-            'https://i.pinimg.com/736x/29/2a/87/292a877c6aad678dabac032fd80bdb72.jpg',
-            'https://i.pinimg.com/736x/29/2a/87/292a877c6aad678dabac032fd80bdb72.jpg',
-            'https://i.pinimg.com/736x/21/31/f3/2131f3d49a56527ff2915c8963dac83d.jpg',
-            'https://i.pinimg.com/736x/21/31/f3/2131f3d49a56527ff2915c8963dac83d.jpg',
-            'https://i.pinimg.com/736x/c9/ef/dc/c9efdc80f57ed7a7a4301680aeddfaab.jpg',
-            'https://i.pinimg.com/736x/ef/46/fd/ef46fdbd80d6bb60157dcbdf5369dc8e.jpg',
-            'https://i.pinimg.com/736x/cc/eb/fe/ccebfe971ef349f14df77a8b687a1a71.jpg',
-            'https://i.pinimg.com/736x/cc/eb/fe/ccebfe971ef349f14df77a8b687a1a71.jpg',
-            'https://i.pinimg.com/736x/cc/eb/fe/ccebfe971ef349f14df77a8b687a1a71.jpg',
-            'https://i.pinimg.com/736x/16/e5/c3/16e5c36496b8f440f991217aae97ae78.jpg',
-            'https://i.pinimg.com/736x/16/e5/c3/16e5c36496b8f440f991217aae97ae78.jpg',
-            'https://i.pinimg.com/736x/73/c5/5c/73c55c10eed682c03727b39ef4075504.jpg'
+    'https://i.pinimg.com/736x/18/36/5c/18365c5ebe02b5ac2fac228f60f2e864.jpg',
+    'https://i.pinimg.com/736x/18/36/5c/18365c5ebe02b5ac2fac228f60f2e864.jpg',
+    'https://i.pinimg.com/736x/33/5e/4c/335e4caed499f52d33a117c166715a1d.jpg',
+    'https://i.pinimg.com/736x/33/5e/4c/335e4caed499f52d33a117c166715a1d.jpg',
+    'https://i.pinimg.com/736x/ff/a6/e9/ffa6e9d7d296ce9ad45ff62b3a61d741.jpg',
+    'https://i.pinimg.com/736x/ff/a6/e9/ffa6e9d7d296ce9ad45ff62b3a61d741.jpg',
+    'https://i.pinimg.com/736x/ca/17/85/ca1785ef2e957e5bbbcf7c767eefc7e2.jpg',
+    'https://i.pinimg.com/736x/ec/1b/16/ec1b1600f90eddcede9f865671e31fb6.jpg',
+    'https://i.pinimg.com/736x/ee/db/22/eedb228b057279e3cabebfcef7ebbdf1.jpg',
+    'https://i.pinimg.com/736x/ee/db/22/eedb228b057279e3cabebfcef7ebbdf1.jpg',
+    'https://i.pinimg.com/736x/ee/b7/7f/eeb77fb88ceb4731b01bd394760cade1.jpg',
+    'https://i.pinimg.com/736x/ee/b7/7f/eeb77fb88ceb4731b01bd394760cade1.jpg',
+    'https://i.pinimg.com/736x/ee/b7/7f/eeb77fb88ceb4731b01bd394760cade1.jpg',
+    'https://i.pinimg.com/736x/29/2a/87/292a877c6aad678dabac032fd80bdb72.jpg',
+    'https://i.pinimg.com/736x/29/2a/87/292a877c6aad678dabac032fd80bdb72.jpg',
+    'https://i.pinimg.com/736x/29/2a/87/292a877c6aad678dabac032fd80bdb72.jpg',
+    'https://i.pinimg.com/736x/21/31/f3/2131f3d49a56527ff2915c8963dac83d.jpg',
+    'https://i.pinimg.com/736x/21/31/f3/2131f3d49a56527ff2915c8963dac83d.jpg',
+    'https://i.pinimg.com/736x/c9/ef/dc/c9efdc80f57ed7a7a4301680aeddfaab.jpg',
+    'https://i.pinimg.com/736x/ef/46/fd/ef46fdbd80d6bb60157dcbdf5369dc8e.jpg',
+    'https://i.pinimg.com/736x/cc/eb/fe/ccebfe971ef349f14df77a8b687a1a71.jpg',
+    'https://i.pinimg.com/736x/cc/eb/fe/ccebfe971ef349f14df77a8b687a1a71.jpg',
+    'https://i.pinimg.com/736x/cc/eb/fe/ccebfe971ef349f14df77a8b687a1a71.jpg',
+    'https://i.pinimg.com/736x/16/e5/c3/16e5c36496b8f440f991217aae97ae78.jpg',
+    'https://i.pinimg.com/736x/16/e5/c3/16e5c36496b8f440f991217aae97ae78.jpg',
+    'https://i.pinimg.com/736x/73/c5/5c/73c55c10eed682c03727b39ef4075504.jpg'
 
 
-        ];
+];
 
 let currentQuestionIndex = 0;
 let responses = new Array(questions.length).fill(null);
@@ -177,9 +177,9 @@ function displayQuestion() {
     questionContainer.innerHTML = `
         <h3 class="question">${questions[currentQuestionIndex].question}</h3>
         <ul class="options">
-            ${questions[currentQuestionIndex].answers.map((answer, index) => 
-                `<li class="option" ><input class="inputFor" type="radio"name="answer" value="${index}" id="answer${index}" ${responses[currentQuestionIndex] === index ? 'checked' : ''}><label for="answer${index}">${answer}</label></li>`
-            ).join('')}
+            ${questions[currentQuestionIndex].answers.map((answer, index) =>
+        `<li class="option" ><input class="inputFor" type="radio"name="answer" value="${index}" id="answer${index}" ${responses[currentQuestionIndex] === index ? 'checked' : ''}><label for="answer${index}">${answer}</label></li>`
+    ).join('')}
         </ul>
     `;
     updateImage();
@@ -234,7 +234,7 @@ function displayResult() {
         <p>Emotional Turmoil: ${scores.emotionalTurmoil.toFixed(2)}%</p>
         <div class="relaventWrapper">
         <p><b>Here are some people with Similar Experiences</b></p>
-    ` + relaventExperience()+"</div>" + `
+    ` + relaventExperience() + "</div>" + `
     <a href='https://www.thecleandivorce.com/book-online' style='display: inline-block; padding: 10px 20px; background-color: #007BFF; color: white; text-align: center; text-decoration: none; border-radius: 5px;'>Free Consultation</a>
     <button id="share-result-btn" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: white; text-align: center; text-decoration: none; border-radius: 5px;">Share Result</button>
     <h1 onClick='showRoadmap()'>Show My Roadmap</h1>`;
@@ -245,11 +245,11 @@ function displayResult() {
     document.getElementById('navigation-container').style.display = 'none';
 }
 
-function showRoadmap(){
+function showRoadmap() {
     const Roadmap = document.querySelector('.roadmapPage');
     Roadmap.classList.add('after');
 }
-function hideRoadmap(){
+function hideRoadmap() {
     const Roadmap = document.querySelector('.roadmapPage');
     Roadmap.classList.remove('after');
 }
@@ -282,16 +282,16 @@ function sendEmail() {
         });
 }
 
-function relaventExperience(){
+function relaventExperience() {
     const scores = calculateScores(responses);
-    if (scores.caringHost>scores.emotionalTurmoil && scores.caringHost>scores.financialBurdens){
-        return("<p>Caring Host</p><ul><li>A caring host ensures guests feel welcomed and comfortable from the moment they arrive.</li><li>Attention to detail, such as clean spaces and thoughtful amenities, enhances the guest experience.</li><li>Personal touches, like remembering guests' preferences and special occasions, show genuine care.</li><li>Effective communication and prompt responses to requests or issues build trust and satisfaction.</li><li>Offering a warm, friendly demeanor and engaging with guests can create a memorable stay.</li></ul>")
+    if (scores.caringHost > scores.emotionalTurmoil && scores.caringHost > scores.financialBurdens) {
+        return ("<p>Caring Host</p><ul><li>A caring host ensures guests feel welcomed and comfortable from the moment they arrive.</li><li>Attention to detail, such as clean spaces and thoughtful amenities, enhances the guest experience.</li><li>Personal touches, like remembering guests' preferences and special occasions, show genuine care.</li><li>Effective communication and prompt responses to requests or issues build trust and satisfaction.</li><li>Offering a warm, friendly demeanor and engaging with guests can create a memorable stay.</li></ul>")
     }
-    else if(scores.emotionalTurmoil>scores.caringHost && scores.emotionalTurmoil>scores.financialBurdens){
-        return("<p>Emotional host</p><ul><li>Divorce causes complex emotions like sadness, anger, fear, guilt, and relief.</li><li>Users often feel like failures, believing they couldn’t sustain their marriage, leading to a drop in self-esteem.</li><li>Societal stigma and fear of judgment from family and friends make it hard to seek help.</li><li>The process can be isolating, making individuals feel alone in their intense emotions and hesitant to reach out for support.</li></ul>")
+    else if (scores.emotionalTurmoil > scores.caringHost && scores.emotionalTurmoil > scores.financialBurdens) {
+        return ("<p>Emotional host</p><ul><li>Divorce causes complex emotions like sadness, anger, fear, guilt, and relief.</li><li>Users often feel like failures, believing they couldn’t sustain their marriage, leading to a drop in self-esteem.</li><li>Societal stigma and fear of judgment from family and friends make it hard to seek help.</li><li>The process can be isolating, making individuals feel alone in their intense emotions and hesitant to reach out for support.</li></ul>")
     }
-    else if(scores.financialBurdens>scores.caringHost && scores.financialBurdens>scores.emotionalTurmoil){
-        return("<p>Finencial Burden</p><ul><li>Divorce brings financial challenges such as living expenses, legal fees, and reduced income.</li><li>Many feel the strain of stricter budgeting and handling new financial responsibilities.</li><li>Financial burdens may include setting up a new household, child support, alimony, and asset division.</li><li>Fear of an uncertain financial future can deter people from leaving an unhappy marriage.</li></ul>")
+    else if (scores.financialBurdens > scores.caringHost && scores.financialBurdens > scores.emotionalTurmoil) {
+        return ("<p>Financial Burdens</p><ul><li>Divorce brings financial challenges such as living expenses, legal fees, and reduced income.</li><li>Many feel the strain of stricter budgeting and handling new financial responsibilities.</li><li>Financial burdens may include setting up a new household, child support, alimony, and asset division.</li><li>Fear of an uncertain financial future can deter people from leaving an unhappy marriage.</li></ul>")
     }
 }
 
@@ -330,7 +330,7 @@ function calculateScores(responses) {
             }
         }
     });
-    
+
 
     return {
         caringHost: (caringHost / totalQuestions) * 100,
